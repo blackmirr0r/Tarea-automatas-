22f5257ecda7e077e551ab2ef8a2f5fbfa9e33d0
Francisco <franciscojavierlevin.m@gmail.com>
2019-05-23 18:06:00 -0400

    Reparación de bugs y agregar funcionalidades

--- afd.py
+++ afd.py
@@ -1,7 +1,7 @@
 #AFD que desde usuario debe ingresar estado inicial 
 #Estados finales, alfabeto y transiciones 
 import sys 
-from tkinter import * 
+from tkinter import *  #LIBRERIA AUN NO USADA
 
 
 ##############################################
@@ -41,6 +41,12 @@ def AcceptstatesValid(states, s):
         entr = input(s).strip()
 
 
+#funcion que devuelve simbolos que no estan en el ALFABETO
+def notInAlphabetSymbols(pal, alphabet):
+    pal = sorted(set("".join(pal)))
+    not_in_alphabet = [symbol for symbol in pal if symbol not in alphabet]
+    return " - ".join(not_in_alphabet)
+
 ###################################################################
 #                   Ingreso de estados                            #
 #   (Si no ingresa nada o solo espacios termina de ingresar)      #
@@ -76,39 +82,68 @@ print()
 
 transitions = {} # diccionario de la forma (estado, simbolo): estado llegada 
 
-tuple_trans = input("Ingrese par Estado, Simbolo (Termina si no ingresa nada): ")
+#string estado, simbolo que luego será tupla 
+tupla = input("Ingrese par Estado, Simbolo (Termina si no ingresa nada): ")
 
-while tuple_trans:
-    tuple_trans = [i.strip(' ') for i in tuple_trans.split(',')] #entrada limpia de espacios para validar correctamente
-    while len(tuple_trans) != 2 or tuple_trans[0] not in states:
+while tupla:}
+    #arreglo de string tupla para eliminar caracteres indeseados
+    arr_tupla = [i.strip() for i in tupla.split(',') if i] 
+    
+    #verifica si arr_tupla es de la forma estado,simbolo
+    while len(arr_tupla) != 2 or arr_tupla[0] not in states or arr_tupla[1] in states:
         print("Entrada invalida")
-        tuple_trans = input("Ingrese par Estado, Simbolo (Termina si no ingresa nada): ")
-        tuple_trans = [i.strip(' ') for i in tuple_trans.split(',')]
+        tupla = input("Ingrese par Estado, Simbolo (Termina si no ingresa nada): ")
+        arr_tupla = [i.strip(' ') for i in tupla.split(',')]
+        if not(tupla): break
+        
+    if not(tupla): break
 
+    #Estado de llegada 
     arrive_state = input("Ingrese Estado de llegada: ")
     while arrive_state not in states:
         print("Estado invalido")
         arrive_state = input("Ingrese Estado de llegada: ")
 
     #agregar clave - valor al diccionario 
-    transitions[tuple(tuple_trans)] = arrive_state 
+    transitions[tuple(arr_tupla)] = arrive_state 
     print(transitions)
     print()   
-    tuple_trans = input("Ingrese par Estado, Simbolo (Termina si no ingresa nada): ")
+    tupla = input("Ingrese par Estado, Simbolo (Termina si no ingresa nada): ")
     
 print()
+
+
+######################################################################
+#          Construcción Alfabeto y Verificación de Aceptacion AFD
+######################################################################
+
+#construir alfabeto en base a simbolos ingresados en transiciones
+alphabet = [symbol[1] for symbol in transitions.keys()]
+
 pal = input("Ingrese la palabra a revisar: ")
-estado_actual = init_state
-for i in pal:
-    if (estado_actual, i) in transitions.keys(): #revisa si existe una transición
-        estado_actual = transitions[(estado_actual, i)]
-    else:
-        print("Palabra no aceptada")
-        break
-if estado_actual in accept_states:
-    print("Palabra aceptada")
+
+#NO SE SI SE PUEDE INGRESAR NADA PERO SI SE PUEDE COMENTEN EL WHILE
+while not(pal):
+    pal = input("Ingrese la palabra a revisar: ")
+
+estado_actual = init_state 
+#revisa si cada simbolo de la palabra existe en alfabeto
+word_accepted = all(elem in alphabet for elem in set(pal)) 
+
+if word_accepted:
+    for i in pal:
+        if (estado_actual, i) in transitions.keys(): #revisa si existe una transición
+            estado_actual = transitions[(estado_actual, i)]
+        else:
+            print("Palabra no aceptada")
+            break
+    
+    if estado_actual in accept_states:
+        print("Palabra aceptada")
+    
 else:
-    print("Palabra no aceptada")    
-
-
+    #funcion para filtrar elementos que no estan en alfabeto
+    not_alphabet = notInAlphabetSymbols(pal, alphabet)
+    print("Palabra no aceptada ya que los simbolos ( "+ not_alphabet+" ) no existen en el alfabeto del AFD")
  
+  
